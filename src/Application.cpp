002ed5ca3b8f43e61261cbb0691720f21aceeec4
ChenFeng-z <896512290@qq.com>
2026-03-05 15:54:17 +0800

    37.Initial_Project_Files

---
+++ src/Application.cpp
@@ -0,0 +1,57 @@
+#include "Application.h"
+
+bool Application::IsRunning() {
+    return running;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Setup function (executed once in the beginning of the simulation)
+///////////////////////////////////////////////////////////////////////////////
+void Application::Setup() {
+    running = Graphics::OpenWindow();  // 系统调用，通过Graphics类打开窗口并返回是否成功打开的结果
+
+    // TODO: setup objects in the scene
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Input processing
+///////////////////////////////////////////////////////////////////////////////
+void Application::Input() {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_QUIT:
+                running = false;
+                break;
+            case SDL_KEYDOWN:
+                if (event.key.keysym.sym == SDLK_ESCAPE)
+                    running = false;
+                break;
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Update function (called several times per second to update objects)
+///////////////////////////////////////////////////////////////////////////////
+void Application::Update() {
+    // TODO: update all objects in the scene
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Render function (called several times per second to draw objects)
+///////////////////////////////////////////////////////////////////////////////
+void Application::Render() {
+    Graphics::ClearScreen(0xFF056263);
+    Graphics::DrawFillCircle(200, 200, 40, 0xFFFFFFFF);
+    Graphics::RenderFrame();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Destroy function to delete objects and close the window
+///////////////////////////////////////////////////////////////////////////////
+void Application::Destroy() {
+    // TODO: destroy all objects in the scene
+
+    Graphics::CloseWindow();
+}
