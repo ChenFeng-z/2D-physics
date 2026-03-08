7f8cd4c6bad29fc7c3e78a3618a3a2ad63165aed
ChenFeng-z <896512290@qq.com>
2026-03-08 16:36:28 +0800

    78.添加了shape.h类

---
+++ src/Physics/Shape.h
@@ -0,0 +1,49 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+
+#include "Vec2.h"
+#include <vector>
+
+enum ShapeType {
+    CIRCLE,
+    POLYGON,
+    BOX
+};
+
+struct Shape{
+    virtual ~Shape() = default;
+    virtual ShapeType GetType() const = 0;
+};
+
+struct CircleShape : public Shape {
+    float radius;
+
+    CircleShape(const float radius);
+    virtual ~CircleShape();
+
+    ShapeType GetType() const override;
+        
+};
+
+struct PolygonShape : public Shape {
+    std::vector<Vec2> vertices;
+
+    PolygonShape(const std::vector<Vec2>& vertices);
+    virtual ~PolygonShape();
+
+    ShapeType GetType() const override;
+};
+
+struct BoxShape:public PolygonShape{
+    float width;
+    float height;
+
+    BoxShape(const float width, const float height);
+    virtual ~BoxShape();
+    ShapeType GetType() const override;
+};
+
+#endif
+
+    
+
